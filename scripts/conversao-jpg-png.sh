18d696516ee69625136d7d6fb8070f44c85d81c4
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-12-15 10:50:06 -0300

    Convertendo arquivos e movendo para o diretorio png

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 cd ~/bash-script/imagens-livros
+
+#verificando se o diretorio png existe
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
 for imagem in *.jpg
 do
   imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1 }')
-	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
