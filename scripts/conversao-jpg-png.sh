18d8ea7c8314345132889dae613a102aa81784ff
KelvinLima <kelvinlimasouza226@gmail.com>
2022-12-18 12:20:44 -0300

    Passando status de saida da conversao

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+converte_imagem(){
+
 cd ~/bash-script/imagens-livros
 
 #verificando se o diretorio png existe
@@ -10,6 +12,18 @@ fi
 
 for imagem in *.jpg
 do
-  imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1 }')
+  local imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1 }')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+
+}
+
+converte_imagem
+
+#verifica se a função converte_imagem funcionou 1 = true & 0 = false
+if [ $? -eq 0 ]
+then
+  echo "Conversao realizada com sucesso"
+else
+  echo "Houve uma falha no processo"
+fi
